fa44b0395c52b74702fab3dcb5e8c2d2e8444329
Rafael Verissimo da Silva <rver_silva@magazineluiza.intranet>
2024-10-23 10:48:24 -0300

    Alterações nas rotas do openAPI

--- server/src/routes/plant.js
+++ server/src/routes/plant.js
@@ -1,6 +1,5 @@
-import { Router } from 'express'
+const express = require('express')
 
+const router = express.Router()
 
-const router = Router()
-
-export default router;
+module.exports = router;
