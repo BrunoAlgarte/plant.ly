e631bad06d35b729731707f25112f141e9bcc486
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2024-10-30 21:43:49 -0300

    primeiras reqs e fix do package e cors

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,14 +1,56 @@
 "use client";
 import { Button } from "../components/ui/button";
-// import { toast, Bounce } from "react-toastify";
+import { toast, Bounce } from "react-toastify";
 import { Input } from "../components/ui/input";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 // import Footer from "../components/footer";
 import Image from "next/image";
-// import axios from "axios";
+import axios from "axios";
 import React from "react";
 
 export default function Home() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const id = '67195a784f728be2f12ebc09';
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3030/v1/users/${id}`, {
+      });
+      console.log(response);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }        
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:3030/v1/auth/login", {
+        email,
+        password,
+      });
+      if (response.status === 200) {
+        toast.success("Login efetuado com sucesso!", {
+          position: "top-center",
+          transition: Bounce,
+        });
+        window.location.href = "/main";
+      }
+    }
+    catch (error) {
+      console.log(error);
+      toast.error("Usuário ou senha inválidos!", {
+        position: "top-center",
+        transition: Bounce,
+      });
+    }
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <>
@@ -28,24 +70,23 @@ export default function Home() {
               placeholder="Usuário"
               className="mt-2 w-10/12 lg:w-1/2 mb-1 rounded-xl py-6 border-2 border-green-800 bg-[#ffffff87]"
               required
-              // value={email}
-              // onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <Input
               type="password"
               placeholder="Senha"
               className="mt-2 w-10/12 lg:w-1/2 mb-6 rounded-xl py-6 border-2 border-green-800 bg-[#ffffff87]"
               required
-              // value={password}
-              // onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <Button
               className="w-1/2 h-10 bg-[#3e8721] hover:bg-[#29581f] text-white shadow-md rounded-xl hover:shadow-xl text-lg"
               type="submit"
               variant="default"
               size="default"
-              // onClick={handleLogin}
-              onClick={() => window.location.href = "/main"}
+              onClick={() => handleLogin()}
             >
               Login
             </Button>
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 
