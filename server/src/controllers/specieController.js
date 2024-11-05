a6dab881ed8b75e9de338c76c1349b30ac6af315
Bruno Algarte Inacio <br_inacio@magazineluiza.intranet>
2024-11-04 21:09:53 -0300

    Rota busca  spieces/name

--- server/src/controllers/specieController.js
+++ server/src/controllers/specieController.js
@@ -1,25 +1,22 @@
 const Species = require('../models/PlantSpecies');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const controller = {}
 
-// Buscar dados de uma espécie po ID
-controller.findById = async (req, res) => {
-    const id = req.params.id;
-    if (!mongoose.isValidObjectId(id)) {
-        return res.status(400).json({ message: "ID da espécie inválido" });
-    }
+
+// Buscar dados de uma espécie por nome
+controller.findByName = async (req, res) => {
+    const name = req.params.name; // Obtendo o nome da espécie a partir dos parâmetros da requisição
 
     try {
-        const species = await Species.findById(id);
+        const species = await Species.findOne({ name }); // Buscando a espécie pelo nome
         if (!species) {
-        return res.status(404).json({ message: 'Espécie não encontrada' });
+            return res.status(404).json({ message: 'Espécie não encontrada' });
         }
         res.status(200).json(species);
     } catch (error) {
         res.status(500).json({ message: 'Erro ao buscar dados da espécie', error });
     }
 };
-
 
 module.exports = controller;
--- server/src/routes/speciesRoutes.js
+++ server/src/routes/speciesRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/specieController')
+const controller = require('../controllers/specieController');
 
-router.get('/:id', controller.findById)
-
+// Rota para buscar a espécie pelo nome
+router.get('/name/:name', controller.findByName); // Aqui estamos definindo a nova rota
 
 module.exports = router;
